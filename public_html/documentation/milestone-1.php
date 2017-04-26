ebcd49ca928a69fd3613fc3a6507dfad02c1ce97
dmartin61@cnm.edu <danielleislesmartin@gmail.com>
2017-04-26 16:24:10 -0600

    added purpose and such in paragraph, WIP

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -4,17 +4,13 @@
 		<title> ID </title>
 	</head>
 	<body>
-		<h1>Purpose:</h1>
+		<h2>Purpose, Audience & Goal:</h2>
 <p>
-	This project is my professional portfolio for photography and web Development
+	The purpose of my personal website is to generate an online presence for my work: photography and web development. My target audience will be perspective clients and employers, therefore my site must be professional and showcase my skills in both areas. The goals for my site is are to attract clients for both photography and web development. I want people to see my work on any device.
 </p>
-		<h1>Audience:</h1>
+		<h2>Persona:</h2>
 <p>
-	My site will be focused toward perspective clients and employers in mind. People who are considering hiring me.
-</p>
-		<h1>Goal:</h1>
-<p>
-	To establish an online presence and begin my digital brand for myself.
+
 </p>
 	</body>
 </html>
