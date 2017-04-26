adbce308df80b835b3a0f5be1c0035b3e41db6f2
dmartin61@cnm.edu <danielleislesmartin@gmail.com>
2017-04-26 16:59:40 -0600

    Work in progress

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -6,9 +6,9 @@
 	<body>
 		<h2>Purpose, Audience & Goal:</h2>
 <p>
-	The purpose of my personal website is to generate an online presence for my work: photography and web development. My target audience will be perspective clients and employers, therefore my site must be professional and showcase my skills in both areas. The goals for my site is are to attract clients for both photography and web development. I want people to see my work on any device.
+	The purpose of my personal website is to generate an online presence for my work: photography and web development. My target audience will be perspective clients and employers, therefore my site must be professional and showcase my skills in both areas. The goals for my site are to attract clients for both photography and web development. I want people to see my work on any device.
 </p>
-		<h2>Persona:</h2>
+		<h2>Persona's:</h2>
 <p>
 
 </p>
