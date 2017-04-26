c94b4f384960b28a9817259b9e1f50fac482139a
dmartin61@cnm.edu <danielleislesmartin@gmail.com>
2017-04-26 15:54:46 -0600

    Started personal website milestone 1

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title> ID </title>
+	</head>
+	<body>
+		<h1>Purpose:</h1>
+<p>
+	This project is my professional portfolio for photography and web Development
+</p>
+		<h1>Audience:</h1>
+<p>
+	My site will be focused toward perspective clients and employers in mind. People who are considering hiring me.
+</p>
+		<h1>Goal:</h1>
+<p>
+	To establish an online presence and begin my digital brand for myself.
+</p>
+	</body>
+</html>
