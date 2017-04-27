662fe4f771ed206e6046143f4ee9398c11e6c061
dmartin61@cnm.edu <danielleislesmartin@gmail.com>
2017-04-26 22:18:05 -0600

    Work in progress

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -1,16 +1,21 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<title> ID </title>
+		<title>Isles Designs</title>
+			<meta charset="UTF-8">
 	</head>
 	<body>
-		<h2>Purpose, Audience & Goal:</h2>
+		<h2>Purpose, Audience & Goal</h2>
 <p>
 	The purpose of my personal website is to generate an online presence for my work: photography and web development. My target audience will be perspective clients and employers, therefore my site must be professional and showcase my skills in both areas. The goals for my site are to attract clients for both photography and web development. I want people to see my work on any device.
 </p>
-		<h2>Persona's:</h2>
+		<h2>Persona & UseCase 1</h2>
 <p>
-
+	Cherish, 32, is planning on getting married on June 3rd 2018 and she heard on social media that I am a photographer and is interested in hiring me for that weekend. She mostly web browses on her iPhone 5 with safari and she is thinking of hiring me as her wedding photographer. She works for the government and lives comfortably on the west side of town with her fiance. She's looking for some of my photos featuring couples in weddings and engagement sessions. Cherish is looking for someone with an artistic style who will fit in like she's part of the family at the wedding. She would like someone with a warm personality but also very professional. She wants photos that she will keep forever, in scrapbooks, frams on the mantel and to post on social media.
+</p>
+		<h2>Persona & UseCase 2</h2>
+<p>
+	John Smith, 45, is the CEO of a DevelopIt! INC. a web development start-up and needs another Junior Developer to add to the team to help with the workload. he will be on his fancy expense PC desktop running Linux OS and google Chrome looking at webpages of perspective employers in the 30 minutes of break he as before interviewing potential candidates. John is looking for a team player with basic technical programming skills as well as an artistic style. Someone who's skills are diverse but most importantly understands back to front web development, programming languages and both Mac and Windows OS systems. Preferably someone who is self motivated, a team player and willing to learn
 </p>
 	</body>
 </html>
