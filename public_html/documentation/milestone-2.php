d4c96d64bd5bb50a30bb1a5fa80a03f04a238ad2
dmartin61@cnm.edu <danielleislesmartin@gmail.com>
2017-05-12 15:28:24 -0600

    started html template for milestone2

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -1,1 +1,16 @@
-<?php
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8"/>
+		<title>
+			Milestone 2
+		</title>
+	</head>
+	<body>
+		<main>
+		<h2>Wireframes</h2>
+			<p>Isles Designs</p>
+			<src img="Front page.png"
+		</main>
+	</body>
+</html>
