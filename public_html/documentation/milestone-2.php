ca57374f7d651b37c11f46139553021ccc6ebdab
dmartin61@cnm.edu <danielleislesmartin@gmail.com>
2017-05-13 14:40:55 -0600

    added photos to images directory of balsamiq wireframs full layouts

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -3,14 +3,17 @@
 	<head>
 		<meta charset="UTF-8"/>
 		<title>
-			Milestone 2
+			Milestone 2a
 		</title>
 	</head>
 	<body>
 		<main>
 		<h2>Wireframes</h2>
 			<p>Isles Designs</p>
-			<src img="Front page.png"
 		</main>
+		<div>
+			<img src="" alt="full browser layout">
+			<img src="" alt="full mobile layout"
+		</div>
 	</body>
 </html>
