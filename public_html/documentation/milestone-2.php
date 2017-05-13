92e8b531d1bc63e39e53345d62d55aa3739f2695
dmartin61@cnm.edu <danielleislesmartin@gmail.com>
2017-05-13 15:53:31 -0600

    added some short paragraphs

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -10,11 +10,17 @@
 		<main>
 		<h2>Wireframes</h2>
 			<p>Isles Designs</p>
+			<p> The site will be one page with the following sections:</p>
+			<p>
+				Home Page: The top of the page will have the title, a nav bar to direct a user to the different sections, and a slide show
+			</p>
+			<p>
+				About Me Section: I would like the title to swing left with a circular image of me on the right. As well as a short and sweet text about me like an elevator speech.
+			</p>
 		</main>
 		<div>
-			<img src="" alt="full browser layout">
-			<img src="" alt="full mobile layout"
-
+			<img src="images/full-browser-wireframe.png" alt="full browser layout">
+			<img src="images/full-moible-wireframe.png" alt="full mobile layout">
 		</div>
 	</body>
 </html>
