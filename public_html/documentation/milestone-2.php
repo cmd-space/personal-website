3cf4e45b57cd77f8ec128ff4750c00c73d5c4423
dmartin61@cnm.edu <danielleislesmartin@gmail.com>
2017-05-14 21:37:00 -0600

    finished writing hte sections and added a little bootstrap to make things look nice

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -2,25 +2,49 @@
 <html>
 	<head>
 		<meta charset="UTF-8"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 		<title>
 			Milestone 2a
 		</title>
 	</head>
 	<body>
 		<main>
-		<h2>Wireframes</h2>
-			<p>Isles Designs</p>
-			<p> The site will be one page with the following sections:</p>
-			<p>
-				Home Page: The top of the page will have the title, a nav bar to direct a user to the different sections, and a slide show
-			</p>
-			<p>
-				About Me Section: I would like the title to swing left with a circular image of me on the right. As well as a short and sweet text about me like an elevator speech.
-			</p>
+		<h1 class="text-center">Wireframes</h1>
+			<blockquote class="blockquote, text-center">Isles Designs</blockquote>
+			<blockquote class="blockquote"> The site will be one page with the following sections:</blockquote>
+			<ul>
+				<li>Home page and Header</li>
+				<li>About me section</li>
+				<li>Projects</li>
+				<li>Contact me</li>
+			</ul>
+			<blockquote class="blockquote">
+				<strong>Home Page: </strong>The header/top of the page will have the title Isles Designs, a nav bar to direct a user to the 4 different sections, and a slide show images of my photography using fancybox3. I have a design in mind for the header "Isles" part of the title will be in a google font Lover's Quarrel font which is a nice cursive and will swing to the left. On the row right below the first part of the title will be Designs in google font Offside font which is different rounded block like text. The first line of text with Isles right where the text ends a new column begins which will house the "Designs" text in the next row. For the Photography slide-show, I want to show a few select photos of my work that will appear in a slide show format.
+			</blockquote>
+			<blockquote class="blockquote">
+				<strong>About Me Section:</strong> I would like the title to swing left with a circular image of me on the right, same image I use as my profile. As well as a short and sweet text paragraph about me like an elevator speech. "Hello I am Danielle a full-stack website developer and photographer. I graduated in 2015 with my Bachelor's of Science in Biochemisty and I'm looking for a company to grow my developer skills in." This section should be simple and easily scale to a mobile device view.
+			</blockquote>
+			<blockquote class="blockquote">
+				<strong>Projects: </strong> The projects section will showcase the projects I have worked, or currently working on. All will have a title, image, short description and a link to the code in Github. For the Capstone project my group and I discussed at one point filming the Raspberry-pi mirror working sort of a how-to use the mirror video and using the video instead of an images to showcase the project. I'm not sure if putting a video in is a stretch goal but a goal none the less. This section will be a challenge to keep even and looking nice with bootstrap while transitioning from a large screen to a mobile device.
+			</blockquote>
+			<blockquote class="blockquote">
+				<strong>Contact me:</strong> This section contains two parts. 1.) The icon website links 2.) contact email template section. These icons I hope to be links directing users to Github, Linkedin, and facebook (or something else). The Contact section I would like to use to hide my email from users but a place where users can email me directly if they like without actually knowing my email. Not too familiar with the the encryption involved with that but that is the plan. The icons for the first part will require some bootstrap to keep them even and on the same line.
+			</blockquote>
+			<blockquote class="blockquote-reverse">
+				<strong>Side Notes:</strong> Visually I plan to separate the sections by different colors, like a light and dark grey instead of black lines. I plan the header section to have some blocks of cyan color behind the Isles title section it was difficult to show my idea using the balsamiq wireframe program but I will keep it as a stretch goal. Thank you for reading I hope I can put my skills to the test and make my personal website great.
+			</blockquote>
 		</main>
 		<div>
-			<img src="images/full-browser-wireframe.png" alt="full browser layout">
-			<img src="images/full-moible-wireframe.png" alt="full mobile layout">
+			<img class="img-fluid" src="images/full-browser-wireframe.png" alt="full browser layout">
+			<img class="img-fluid" src="images/full-moible-wireframe.png" alt="full mobile layout">
 		</div>
 	</body>
 </html>
